d06cb738ba83bfd091b8c29739a7d25fa74ade79
ThonyFlores <anthony.g91@icloud.com>
2023-08-25 13:25:15 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TestAplication',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
